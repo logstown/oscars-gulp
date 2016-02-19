09455f8926a57ee613b3a050f26200ce694b40e3
Logan Joecks <ljoecks@gmail.com>
2016-02-18 23:15:24 -0500

    add pool competitors to home screen

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,41 +8,65 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController(TimeService, Auth, $firebaseObject, FBUrl, User, $modal, PoolService) {
+    function MainController(TimeService, Auth, $firebaseObject, FBUrl, User, $modal, PoolService, $scope) {
         var vm = this;
         var ref = new Firebase(FBUrl);
+        var uid = Auth.$getAuth().uid;
 
         vm.pool = undefined;
+        vm.noPool = false;
 
         vm.isAfterOscarStart = TimeService.isAfterOscarStart;
         vm.oscarStart = TimeService.getOscarStart();
         vm.createNewPool = createNewPool;
 
-
         activate();
 
+        $scope.$on('add.pool.hide', function() {
+            loadUserAndPool();
+        })
+
         function activate() {
-            var user = User(Auth.$getAuth().uid);
+            loadUserAndPool();
+        }
+
+        function loadUserAndPool() {
+            var user = User(uid);
 
             user.$loaded()
                 .then(function() {
-                    console.log(user)
                     if (user.poolId) {
+                        vm.users = [];
                         vm.pool = $firebaseObject(ref.child('pools').child(user.poolId));
+                        vm.poolUrl = POOL_URL + user.poolId;
+
                         vm.pool.$loaded()
                             .then(function() {
+                                var usersRef = ref.child('users');
+                                var competitorsRef = ref.child('pools').child(user.poolId).child('competitors');
+
+                                competitorsRef.on('child_added', function(snap) {
+                                    usersRef.child(snap.key()).once('value', function(deal) {
+                                        vm.users.push(deal.val())
+                                    })
+                                })
+
 
                             })
+                    } else {
+                        vm.noPool = true;
                     }
                 })
         }
 
         function createNewPool() {
+
             $modal({
                 templateUrl: 'app/pool/_addPool.html',
                 show: true,
                 controllerAs: 'vm',
                 animation: 'am-fade-and-scale',
+                prefixEvent: 'add.pool',
                 controller: function() {
                     var newPool = this;
 
