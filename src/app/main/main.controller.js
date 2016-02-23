e532475f0181a709eb4663c4ab8bdce7983cbc3d
Logan Joecks <RC05196@BOS0257.ryanco.com>
2016-02-23 13:00:20 -0500

    refactor and add "leave pool" button

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,7 +8,7 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController(TimeService, Auth, $firebaseObject, FBUrl, User, $modal, $scope, PicksService) {
+    function MainController(TimeService, Auth, $firebaseObject, FBUrl, User, $modal) {
         var vm = this;
         var ref = new Firebase(FBUrl);
         var poolsRef = ref.child('pools');
@@ -26,7 +26,9 @@
         vm.getProgressWidth = getProgressWidth;
         vm.getProgressBarColor = getProgressBarColor;
         vm.removeUser = removeUser;
-        vm.showAdmin = showAdmin;
+        vm.inviteOthers = inviteOthers;
+        vm.getInviteColspan = getInviteColspan;
+        vm.leavePool = leavePool;
 
         activate();
 
@@ -44,97 +46,68 @@
             var user = User(vm.currentUid);
 
             user.$loaded()
-                .then(function() {
+                .then(function(user) {
                     if (!user.pools) {
                         vm.noPools = true;
                         return;
                     }
 
-                    usersRef.child(vm.currentUid).child('pools').on('child_added', function(poolIdSnap) {
-                        poolsRef.child(poolIdSnap.key()).once('value', function(poolSnap) {
-                            if (poolSnap.val() === null) {
-                                usersRef.child(vm.currentUid).child('pools').child(poolIdSnap.key()).remove();
-                                vm.noPools = true
-                            } else {
-                                var pool = poolSnap.val();
-                                pool.id = poolIdSnap.key();
-                                pool.users = [];
-                                vm.pools.push(pool)
-
-                                poolsRef.child(poolIdSnap.key()).child('competitors').on('child_added', function(competitorIdSnap) {
-                                    usersRef.child(competitorIdSnap.key()).once('value', function(competitorSnap) {
-                                        var competitor = competitorSnap.val();
-                                        competitor.id = competitorIdSnap.key();
-                                        competitor.progress = 0;
-                                        var poolIdx = _.findIndex(vm.pools, { id: poolIdSnap.key() })
-                                        vm.pools[poolIdx].users.push(competitor);
-
-                                        picksRef.child(competitor.uid).on('child_added', function() {
-                                            var competitorIdx = _.findIndex(vm.pools[poolIdx].users, { id: competitorIdSnap.key() });
-                                            vm.pools[poolIdx].users[competitorIdx].progress++;
-                                        })
-                                    })
-                                })
-
-                                poolsRef.child(poolIdSnap.key()).child('competitors').on('child_removed', function(competitorIdSnap) {
-                                    var poolIdx = _.findIndex(vm.pools, { id: poolIdSnap.key() });
-                                    _.remove(vm.pools[poolIdx].users, { id: competitorIdSnap.key() })
-                                })
-                            }
-                        })
-                    })
-
-                    usersRef.child(vm.currentUid).child('pools').on('child_removed', function(poolIdSnap) {
-                        _.remove(vm.pools, { id: poolIdSnap.key() })
-                    })
-
-                    // vm.users = [];
-                    // vm.pool = $firebaseObject(ref.child('pools').child(user.poolId));
-
-                    // vm.pool.$loaded()
-                    //     .then(function() {
-                    //         if (vm.pool.$value === null) {
-                    //             ref.child('users').child(vm.currentUid).child('poolId').remove();
-                    //             vm.pool.$destroy();
-
-                    //             vm.noPool = true;
-
-                    //             return;
-                    //         }
-
-                    //         var usersRef = ref.child('users');
-                    //         var competitorsRef = ref.child('pools').child(user.poolId).child('competitors');
-                    //         createPoolUrl(user);
-
-                    //         competitorsRef.on('child_added', function(snap) {
-                    //             usersRef.child(snap.key()).once('value', function(competitor) {
-                    //                 competitor = competitor.val();
-
-                    //                 ref.child('picks').child(competitor.uid).once('value', function(userPicks) {
-                    //                     competitor.progress = PicksService.getSize(userPicks.val())
-                    //                     vm.users.push(competitor)
-                    //                 })
-                    //             });
-                    //         });
-
-                    //         competitorsRef.on('child_removed', function(snap) {
-                    //             usersRef.child(snap.key()).once('value', function(competitor) {
-                    //                 _.remove(vm.users, competitor.val());
-                    //             })
-                    //         })
-                    //     });
+                    getPools();
                 });
         }
 
-        function createPoolUrl(user) {
-            vm.poolUrl = POOL_URL + user.poolId;
+        function getPools() {
+            var userPoolsRef = usersRef.child(vm.currentUid).child('pools');
+
+            userPoolsRef.on('child_added', function(poolIdSnap) {
+                var poolId = poolIdSnap.key();
+                var poolRef = poolsRef.child(poolId);
+
+                poolRef.once('value', function(poolSnap) {
+                    var pool = poolSnap.val();
+
+                    if (pool === null) {
+                        userPoolsRef.child(poolId).remove();
+                        vm.noPools = true;
+                    } else {
+                        pool.id = poolId;
+                        pool.users = [];
+                        vm.pools.push(pool);
+
+                        poolRef.child('competitors').on('child_added', function(competitorIdSnap) {
+                            var competitorId = competitorIdSnap.key();
+
+                            usersRef.child(competitorId).once('value', function(competitorSnap) {
+                                var poolIdx = _.findIndex(vm.pools, { id: poolId });
+                                var competitor = competitorSnap.val();
+
+                                competitor.id = competitorId;
+                                competitor.progress = 0;
+                                vm.pools[poolIdx].users.push(competitor);
+
+                                picksRef.child(competitor.uid).on('child_added', function() {
+                                    var competitorIdx = _.findIndex(vm.pools[poolIdx].users, { id: competitorId });
+                                    vm.pools[poolIdx].users[competitorIdx].progress++;
+                                })
+                            })
+                        })
+
+                        poolRef.child('competitors').on('child_removed', function(competitorIdSnap) {
+                            var poolIdx = _.findIndex(vm.pools, { id: poolId });
+                            _.remove(vm.pools[poolIdx].users, { id: competitorIdSnap.key() })
+                        })
+                    }
+                })
+            })
+
+            userPoolsRef.on('child_removed', function(poolIdSnap) {
+                _.remove(vm.pools, { id: poolIdSnap.key() })
+            })
         }
 
         function getPicksSize() {
-
             picksRef.child(vm.currentUid).once('value', function(userPicksSnap) {
                 vm.picksSize = userPicksSnap.numChildren();
-                console.log(vm.picksSize)
             })
         }
 
@@ -158,14 +131,48 @@
             }
         }
 
-        function showAdmin(uid) {
-            return uid === vm.pool.creator;
-        }
-
         function removeUser(user) {
             if (confirm('Confirm removing user from Pool')) {
                 ref.child('pools').child(vm.pool.$id).child('competitors').child(user.uid).remove();
             }
+        }
+
+        function getInviteColspan(pool) {
+            return vm.currentUid === pool.creator ? 4 : 3;
+        }
+
+        function leavePool(pool) {
+            var message = 'Are you sure you want to leave ' + pool.name + '?';
+
+            if (vm.currentUid === pool.creator) {
+                message += ' Note: Since you are the Admin, you will be deleting the entire pool as well.'
+            }
+            if (confirm(message)) {
+                ref.child('pools').child(pool.id).child('competitors').child(vm.currentUid).remove();
+                ref.child('users').child(vm.currentUid).child('pools').child(pool.id).remove();
+
+                if (vm.currentUid === pool.creator) {
+                    ref.child('pools').child(pool.id).remove();
+                }
+            }
+        }
+
+        function inviteOthers(pool) {
+            $modal({
+                animation: 'am-fade-and-scale',
+                container: 'body',
+                templateUrl: 'pool-link.html',
+                locals: {
+                    poolUrl: POOL_URL + pool.id
+                },
+                controllerAs: 'vm',
+                show: true,
+                controller: ['poolUrl', function(poolUrl) {
+                    var share = this;
+
+                    share.poolUrl = poolUrl;
+                }]
+            });
         }
 
         function createNewPool() {
@@ -174,8 +181,6 @@
                 show: true,
                 controllerAs: 'vm',
                 animation: 'am-fade-and-scale',
-                prefixEvent: 'add.pool',
-                scope: $scope,
                 controller: ['Auth', 'PoolService', function(Auth, PoolService) {
                     var newPool = this;
 
